63c26ad4f380c36c8fdfa1d8a78c982f324f2556
Pavel Shakhlovich <pavel.shakhlovich@sap.com>
2023-03-02 21:39:27 +0400

    initial

---
+++ src/main/java/com/pshakhlovich/javamentoringadv/JavaMentoringAdvApplication.java
@@ -0,0 +1,13 @@
+package com.pshakhlovich.javamentoringadv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMentoringAdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMentoringAdvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pshakhlovich/javamentoringadv/JavaMentoringAdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pshakhlovich.javamentoringadv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMentoringAdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
